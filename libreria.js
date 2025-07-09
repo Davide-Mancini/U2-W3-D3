fad63b99131094bc448b533ffa745c6849cc5191
Davide-Mancini <mancinidavide73@gmail.com>
2025-07-09 22:00:23 +0200

    adds

--- libreria.js
+++ libreria.js
@@ -13,13 +13,13 @@ const libreria = function () {
       const colonna = document.getElementById("colonna");
       let html = " ";
       libro.forEach((element) => {
-        html += `<div class="col col-6 col-md-3 col-lg-2 mt-3" id="card" >
-        <img src="${element.img}" class="card-img-top" alt="..." style= 'height:100%'>
-        <div class="card-body " style='height:100%'>
+        html += `<div class=" card col col-6 col-md-3 col-lg-2 mt-3 mx-2 border-danger p-0" id="card" style= 'height:650px'>
+        <img src="${element.img}" class="card-img-top object-fit-contain" alt="..." '>
+        <div class="card-body  d-flex flex-column " style='height:100%'>
           <h5 class="card-title">${element.title}</h5>
-          <p class="card-text">${element.price}</p>
+          <p class="card-text">€${element.price}</p>
           
-          <a href="#" class=" compra btn btn-primary mb-2">COMPRA ORA</a>
+          <a href="#" class=" compra btn btn-primary mb-2 mt-auto ">COMPRA ORA</a>
           <a href="#" class="elimina btn btn-danger" >ELIMINA</a>
         </div>
       </div>
@@ -27,9 +27,7 @@ const libreria = function () {
         console.log(libro[0]);
         colonna.innerHTML = html;
 
-        const bottoneCompra = document.querySelectorAll(
-          "a.btn btn-primary mb-2"
-        );
+        // console.log(bottoneCompra);
         const bottoneElimina = document.querySelectorAll(".elimina");
         bottoneElimina.forEach((bottone) => {
           bottone.addEventListener("click", function (e) {
@@ -38,8 +36,17 @@ const libreria = function () {
             card.classList.add("d-none");
           });
         });
+        const bottoneCompra = document.querySelectorAll(".compra");
+        bottoneCompra.forEach((bottonecompra) => {
+          bottonecompra.addEventListener("click", function (e) {
+            e.preventDefault;
+            const section = document.getElementById("section");
+            const card = this.closest(".card").cloneNode(true);
+
+            section.appendChild(card);
+          });
+        });
       });
-      console.log(bottoneElimina);
     })
     .catch((err) => {
       console.log("ERRORE", err);
